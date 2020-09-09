0ae0ab9a99d1040818fa5a986dcc060633ea5271
Leon Jamison <leonjamison@icloud.com>
2020-09-09 00:18:42 -0400

    ready

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -2,6 +2,8 @@ class Nomination < ApplicationRecord
   belongs_to :user
 
   validates :Title, uniqueness: {message: '%{value} has already been nominated! Please make another selection.'}
+ 
+  # custom validation to try to limit how many objects a user can create 
   # validates :nomination_count_within_limit, :on => :create 
 
   # def nomination_count_within_limit
